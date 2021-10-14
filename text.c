7bf77c1e1bbcf6860c161863c91dec459a2b3ef9
Sakib Noman <skbnmn@gmail.com>
2021-10-14 13:54:42 +0600

    prob1

---
+++ text.c
@@ -0,0 +1,59 @@
+
+#include <stdio.h>
+
+#include <conio.h>
+
+int main()
+
+{
+
+        int a[10000],i,n,key;
+
+
+
+        printf("Enter size of the  array : ");
+
+        scanf("%d", &n);
+
+        printf("Enter elements in array : ");
+
+        for(i=0; i<n; i++)
+
+             {
+
+                scanf("%d",&a[i]);
+
+
+    }
+
+         printf("Enter the key : ");
+
+        scanf("%d", &key);
+
+
+
+        for(i=0; i<n; i++)
+
+             {
+
+                if(a[i]==key)
+
+                    {
+
+            printf("Yes ");
+
+                        return 0;
+
+
+        }
+
+
+
+
+    }
+
+
+
+    printf("No");
+
+}
