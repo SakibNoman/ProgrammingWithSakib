e51c93d7adb5e30c94006f20d7d5ace52dd9666c
Sakib Noman <skbnmn@gmail.com>
2021-10-17 22:18:40 +0600

    func

--- func.c
+++ func.c
@@ -1,21 +1,15 @@
 
-
 #include<stdio.h>
 
 
-void summation(int x,int y,int z){
-    int sum;
-    sum = x+y+z;
-    printf("%d\n",sum);
+int summation(int x,int y,int z){
+    int sum = x+y+z;
+    
 }
 
 int main(){
     summation(1,2,3);
 
-    summation(4,5,6);
-
-    summation(34,5,2);
-
-    summation(5,3,2);
+    printf("%d\n",summation(1,2,3));
     return 0;
 }
